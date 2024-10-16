486b82644fdc97d363f428c99e2da9742519ab5e
Rajarshi Saha <saharajarshi14@gmail.com>
2024-10-16 20:42:11 +0530

    Added a new component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Dummy from './components/dummy'
-
+import Dummy from './components/Dummy'
+import Home from './components/home'
 const App = () => {
   return (
     <div>
       <Dummy/>
+      <Home/>
     </div>
   )
 }
