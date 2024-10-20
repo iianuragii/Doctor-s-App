1d7f53965e5ca7fd58e8335034d72a68ab0eb934
Rajarshi Saha <saharajarshi14@gmail.com>
2024-10-20 19:09:29 +0530

    Minor change

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ require('./Database/dbConnect');
 const { allocation_priority } = require('./Backend/allocation_priority'); 
 
 app.use(cors());
-const port = 5000;
+app.use(express.json());
+const port = 4000;
 
 app.get('/',(req,res)=>{
     const input = ["fatigue", "blackheads", "weight_loss", "acidity"];
@@ -13,9 +14,15 @@ app.get('/',(req,res)=>{
     res.send(`Doctor allocated : ${result}`);  
 })
 
-app.get('/api',(req,res)=>{
-    return res.json({val:"Hola from Backend"});
-})
+// app.get('/api',(req,res)=>{
+//     return res.json({val:"Hola from Backend"});
+// })
+app.post('/api', (req, res) => {
+    const formData = req.body;
+    console.log('Form Data:', formData);
+    res.status(200).json({ message: 'Form data received successfully', formData });
+  });
+  
 
 app.listen(port,()=>{
     console.log(`Server is running at Port ${port}`);
