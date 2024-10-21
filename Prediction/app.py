4224e67de743a9a8047c43f368f42a1203daa5dc
Rajarshi Saha <saharajarshi14@gmail.com>
2024-10-21 20:31:14 +0530

    Minor change in directory path

--- Prediction/app.py
+++ Prediction/app.py
@@ -7,7 +7,7 @@ import numpy as np
 app = Flask(__name__)
 # Step 1: Specify the directory and file name where the model is saved
 
-directory =  'C:\\Users\\Anurag dutta\\Desktop\\Doctor-s-app\\Prediction' # Replace with your directory
+directory =  'D:\\DoctorApp\\Doctor-s-App\\Prediction' # Replace with your directory
 model_filename = 'disease_prediction_model.pkl'
 label_encoder_filename='label_encoder_2.pkl'
 scaler_filename='scaler.pkl'
